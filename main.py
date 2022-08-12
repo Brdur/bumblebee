dee731e9f7ff57c2bd7fb8db149ee2cbbc6e286d
Borodur <kerkker@gmail.com>
2022-08-12 23:57:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def main():
         protect_c = True
     if not os.environ.get('INPUT_DISABLE_NOTIFICATION'):
         disable_n = True
-    print(os.environ.get('INPUT_DISABLE_NOTIFICATION'), os.environ.get('INPUT_PROTECT_CONTENT'))
+    raise Exception(os.environ.get('INPUT_DISABLE_NOTIFICATION'), os.environ.get('INPUT_PROTECT_CONTENT'))
+    print(os.environ.get('INPUT_DISABLE_NOTIFICATION'), os.environ.get('INPUT_PROTECT_CONTENT'), os.environ.get('INPUT_PARSE_MODE'))
     if os.environ.get('INPUT_PARSE_MODE') in ("MarkdownV2", "HTML", "Markdown"):
         parse_m = os.environ.get('INPUT_PARSE_MODE')
     else:
