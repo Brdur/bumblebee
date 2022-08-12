cf1236dbe4d0881e1b510231e3fb356d1a7b5ea4
Borodur <kerkker@gmail.com>
2022-08-13 00:28:30 +0300

    13

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ bot = telebot.TeleBot(os.environ.get("INPUT_BOT_TOKEN"))
 def main():
     protect_c, disable_n, parse_m = None, None, None
     message = os.environ.get('INPUT_MESSAGE')
-    if os.environ.get('INPUT_PROTECT_CONTENT') is not None:
+    if os.environ.get('INPUT_PROTECT_CONTENT'):
         protect_c = True
-    if not os.environ.get('INPUT_DISABLE_NOTIFICATION') is not None:
+    if not os.environ.get('INPUT_DISABLE_NOTIFICATION'):
         disable_n = True
     if os.environ.get('INPUT_PARSE_MODE') in ("MarkdownV2", "HTML", "Markdown"):
         parse_m = os.environ.get('INPUT_PARSE_MODE')
