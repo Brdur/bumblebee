d41a135325ec7fd20ce321442ac9452753adb029
Borodur <kerkker@gmail.com>
2022-08-12 18:03:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 
 def main():
     my_input = os.environ.get("INPUT_MYINPUT")
-    my_input = os.environ.get("INPUT_ASS")
+    my_input2 = os.environ.get("INPUT_ASS")
     my_output = f"Hello {my_input2}"
 
     print(f"::set-output name=myOutput::{my_output}")
